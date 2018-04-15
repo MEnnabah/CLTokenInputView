3063b801cf36efed4c85d0f8238cc31a643ce807
Mohammed M. Ennabah <ennabahm9@gmail.com>
2018-04-15 14:31:33 +0200

    access tokenViews fix

--- CLTokenInputView/CLTokenInputView/CLTokenInputView.h
+++ CLTokenInputView/CLTokenInputView/CLTokenInputView.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 
 #import "CLToken.h"
+#import "CLTokenView.h"
 
 #if __has_feature(objc_generics)
 #define CL_GENERIC_ARRAY(type) NSArray<type>
@@ -105,7 +106,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)removeToken:(CLToken *)token;
 - (nullable CLToken *)tokenizeTextfieldText;
 
-- (NSArray *)allTokenView;
+- (nullable CLTokenView *)allTokenView;
 
 // Editing
 - (void)beginEditing;
